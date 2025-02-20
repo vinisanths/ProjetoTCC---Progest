a3028f82ffe7947a77f57cc3cab4a6652639a03b
vinisanths <vinipsantos0@gmail.com>
2025-02-20 15:06:55 -0300

    Atualiza endereço de email para envio de mensagens de contato

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function enviarEmail(event) {
   const email = document.getElementById("email").value;
   const mensagem = document.getElementById("mensagem").value;
 
-  const mailtoLink = `mailto:suporte@progest.com?subject=Contato de ${nome}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+  const mailtoLink = `mailto:consultoriaprogest9@gmail.com?subject=Contato de ${nome}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
   window.location.href = mailtoLink;
 }
 
