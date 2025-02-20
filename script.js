96ab34288a96c3fcd4c3d448c2671617a7dacea8
vinisanths <vinipsantos0@gmail.com>
2025-02-20 14:48:42 -0300

    Adiciona menu responsivo e melhora o estilo do cabeçalho e do formulário de contato

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function enviarEmail(event) {
-    event.preventDefault();
-  
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const mensagem = document.getElementById("mensagem").value;
-  
-    const mailtoLink = `mailto:suporte@progest.com?subject=Contato de ${nome}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
-    window.location.href = mailtoLink;
-  }
-  
+  event.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const mensagem = document.getElementById("mensagem").value;
+
+  const mailtoLink = `mailto:suporte@progest.com?subject=Contato de ${nome}&body=Nome: ${nome}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+  window.location.href = mailtoLink;
+}
+
+// MENU RESPONSIVO
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+  document.querySelector(".menu").classList.toggle("open");
+});
